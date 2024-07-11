21146597643fd3314e9e50ed07b08df00d25163c
colyon <cdor@protonmail.com>
2024-07-11 05:25:33 -0400

    added SimpleGradeCalculator; simple if-else program to calculate a passing/failing grade

---
+++ SimpleGradeCalculator.py
@@ -0,0 +1,12 @@
+# Grade Calculator
+
+print('Welcome to Grade Calculator')
+print('A score 55 or over is passing')
+print('')
+grade = int(input('Enter your grade:'))
+
+if grade >= 55:
+    print('You passed.')
+
+else:
+    print('You failed.')
